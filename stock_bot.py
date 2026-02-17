93737d3e56ef12b2790ad6fa2129b2cb6e3e2e1c
Justauseruho <Sh1t06635@gmail.com>
2026-02-17 22:24:28 +0300

    Update stock_bot.py

--- stock_bot.py
+++ stock_bot.py
@@ -199,5 +199,6 @@ async def main():
     await init_db()
     await dp.start_polling(bot)
 
-if name == "__main__":
-    asyncio.run(main())
+if __name__ == "__main__":
+    print("Bot started")
+    bot.infinity_polling()
